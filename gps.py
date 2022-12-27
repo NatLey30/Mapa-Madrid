e0620e5995162e9eeb5ba275071968654addaf58
NatLey30 <112613481+NatLey30@users.noreply.github.com>
2022-12-27 01:12:53 +0100

    .

--- gps.py
+++ gps.py
@@ -3,38 +3,72 @@ import grafo
 import networkx as nx
 import matplotlib.pyplot as plt
 import math
+import re
 
 
-def pintar(grafo_madrid):
+def datos(direcciones, direccion):
     '''
-    Pintamos el grafo llamando a la función del grafo que pasa
-    nuestro grafo a un grafo de NetworkX.
-    Args: grafo creado
+    Lee el csv de direcciones y busca las coincidencias con la direccion
+    que se quiere buscar y escoge la primera.
+    Args:
+        direcciones: csv
+        direccion: la que se quiere buscar
+    Return: dir y coordenadas
+    '''
+
+    index = direcciones[direcciones['Direccion completa para el numero'].str.contains(direccion, case=False)].reset_index()
+
+    # Se saca el nombre de la calle
+    cla = index['Clase de la via'][0].strip()
+    par = index['Partícula de la vía'][0].strip()
+    nom = index['Nombre de la vía'][0].strip()
+
+    # Se obtienen las coordenadas
+    coordenada_x = index['Coordenada X (Guia Urbana) cm'][0]
+    coordenada_y = index['Coordenada Y (Guia Urbana) cm'][0]
+
+    # datos
+    dir = cla + ' ' + par + ' ' + nom
+    coordenada = (int(coordenada_x), int(coordenada_y))
+
+    return dir, coordenada
+
+
+def cercano(u, dir, coordenada, grafo_madrid):
+    '''
+    Función que busca el vértice más cercano al vertice que se introduce como
+    argumento y se crea una arista entre estos dos.
+    Args:
+        dir: vertice a añadir
+        coordenada: coordenadas del vertice
+        grafo_madrid: grafo al que se añade
     Return: None
     '''
+    coordenadas_c = (1000000, 1000000)
+    via = ''
+    df = cruces[cruces['Literal completo del vial tratado'] == dir]
 
-    # Sacamos las posiciones de los vértices #
-    pos = {}
-    # Para cada vértice del grafo
+    for index, row in df.iterrows():
+        x = row['Coordenada X (Guia Urbana) cm (cruce)']
+        y = row['Coordenada Y (Guia Urbana) cm (cruce)']
+        resta_x = abs(coordenada[0] - x)
+        resta_y = abs(coordenada[0] - y)
+        if resta_x < coordenadas_c[0] or resta_y < coordenadas_c[0]:
+            coordenadas_c = (x, y)
+
     for v in grafo_madrid.vertices:
-        objeto = grafo_madrid.vertices[v]
-        # en la posición 3 de la lista de datos, se encuentran sus coordenadas
-        coordenadas = objeto.datos[2]
-        pos[v] = coordenadas
-
-    G = grafo_madrid.convertir_a_NetworkX()
-    plot = plt.plot()
-    nx.draw(G, pos=pos, node_size=0.1)
-    plt.show()
+        vertice = grafo_madrid.vertices[v]
+        if vertice.datos[2] == coordenadas_c:
+            grafo_madrid.agregar_arista(u, vertice, None, 1)
+            return
 
 
 if __name__ == "__main__":
-
-    # ABRIMOS ARCHIVOS
+    # ABRIMOS ARCHIVOS #
     cruces = pd.read_csv('cruces.csv', sep=";",  encoding="LATIN_1")
     direcciones = pd.read_csv('direcciones.csv', sep=";",  encoding="LATIN_1", low_memory=False)
 
-    # GRAFOS
+    # GRAFOS #
     grafo_madrid_e = grafo.Grafo(False)
     grafo_madrid_a = grafo.Grafo(False)
 
@@ -109,60 +143,68 @@ if __name__ == "__main__":
             grafo_madrid_e.agregar_arista(v, w, data, dist)
             grafo_madrid_a.agregar_arista(v, w, data, peso)
 
-    # NAVEGADOR
-    grafo_madrid_e = euclidea(cruces)
-    grafo_madrid_a = autovia(cruces)
+    # NAVEGADOR #
 
+    # Pedimos al usuario que introduzca las direcciones de origen y destino
     origen = input("Introduzca la dirección completa del origen tal y como está en la base de datos (Presione intro para finalizar): ")
-    destino = input("Introduzca la dirección completa del origen tal y como está en la base de datos (Presione intro para finalizar): ")
+    destino = input("Introduzca la dirección completa del destino tal y como está en la base de datos (Presione intro para finalizar): ")
 
+    # Mientras estas direcciones no estén vacias
     while origen != '' and destino != '':
 
-        index_o = direcciones[direcciones['Direccion completa para el numero'] == origen].reset_index
-        cla_o = index_o['Clase de la via'][0].strip()
-        par_o = index_o['Partícula de la vía'][0].strip()
-        nom_o = index_o['Nombre de la vía'][0].strip()
+        # datos
+        dir_o, coordenada_o = datos(direcciones, origen)
+        dir_d, coordenada_d = datos(direcciones, destino)
+        print(coordenada_o, coordenada_d)
 
-        coordenada_o_x = index_o['Coordenada X (Guia Urbana) cm'][0]
-        coordenada_o_y = index_o['Coordenada Y (Guia Urbana) cm'][0]
-
-        # datos origen
-        dir_o = cla_o + ' ' + par_o + ' ' + nom_o
-        coordenada_o = (coordenada_o_x, coordenada_o_y)
-
-        index_d = direcciones[direcciones['Direccion completa para el numero'] == destino].reset_index
-        cla_d = index_d['Clase de la via'][0].strip()
-        par_d = index_d['Partícula de la vía'][0].strip()
-        nom_d = index_d['Nombre de la vía'][0].strip()
-
-        coordenada_d_x = index_d['Coordenada X (Guia Urbana) cm'][0]
-        coordenada_d_y = index_d['Coordenada Y (Guia Urbana) cm'][0]
-
-        # datos destino
-        dir_d = cla_d + ' ' + par_d + ' ' + nom_d
-        coordenada_d = (coordenada_d_x, coordenada_d_y)
-
+        # El ususario debe decidir si quiere escoger la ruta
         continuar = True
         while continuar:
-            op = input('Desea encontrar la ruta más corta(1) o más rápida(2): ')
-            if op == 1 or op == 2:
+            op = str(input('Desea encontrar la ruta más corta(1) o más rápida(2): '))
+            if op == '1' or op == '2':
                 continuar = False
             else:
                 print('No ha introducido una opción correcta, o 1 o 2')
 
         if op == 1:
             grafo_madrid = grafo_madrid_e
-
         else:
             grafo_madrid = grafo_madrid_a
 
+        # Añadimos dos vértices nuevos al grafo, el origen y el destino
         grafo_madrid.agregar_vertice('origen')
         grafo_madrid.agregar_vertice('destino')
 
-        print(nom_o)
-        print(nom_d)
+        # Buscamos el vértice del grafo más cercano al y añadimos una arista entre estos dos
+        cercano('origen', dir_o, coordenada_o, grafo_madrid)
 
-        # pintar(grafo_madrid)
+        # Hacemos lo mismo para el destino
+        cercano('destino', dir_d, coordenada_d, grafo_madrid)
+
+        # Buscamos la ruta
+        camino = grafo_madrid.camino_minimo(origen, destino)
+
+        # Pasamos a NetworkX
+        # Sacamos las posiciones de los vértices
+        pos = {}
+        # Para cada vértice del grafo
+        for v in grafo_madrid.vertices:
+            if v != 'origen' or v!= 'destino':
+                objeto = grafo_madrid.vertices[v]
+                # en la posición 3 de la lista de datos, se encuentran sus coordenadas
+                coordenadas = objeto.datos[2]
+                pos[v] = coordenadas
+        pos['origen'] = coordenada_o
+        pos['destino'] = coordenada_d
+
+        G = grafo_madrid.convertir_a_NetworkX()
+
+        # Pintamos
+        plot = plt.plot()
+        nx.draw(G, pos=pos, node_size=0.1, edge_color='k')
+        nx.draw(camino, pos=pos, edge_color='b')
+        nx.draw_networkx_nodes(G, pos=pos, nodelist=['origen', 'destino'], node_size=5, node_color='r')
+        plt.show()
 
         origen = input("Introduzca la dirección completa del origen tal y como está en la base de datos (Presione intro para finalizar): ")
-        destino = input("Introduzca la dirección completa del origen tal y como está en la base de datos (Presione intro para finalizar): ")
+        destino = input("Introduzca la dirección completa del destino tal y como está en la base de datos (Presione intro para finalizar): ")
